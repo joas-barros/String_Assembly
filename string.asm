d7fe03e781f3ab0af8e4d96af0f437f41160ad93
joas-barros <joas.barros14@hotmail.com>
2024-07-28 11:48:48 -0300

    strncat

--- string.asm
+++ string.asm
@@ -3,6 +3,7 @@
 .data
 	nome: .asciiz "Joas "
 	sobrenome: .asciiz "Gabriel"
+	count: .word 3
 .globl main
 .text
 
@@ -85,10 +86,50 @@ saida_strcat:
 	addi $sp, $sp, 12
 	jr $ra #volta ao programa principal
 
+
+strncat:
+	addi $sp, $sp, -16
+	sw $ra, 12($sp)
+	sw $s0, 8($sp)
+	sw $s1, 4($sp)
+	sw $s2, 0($sp)
+	
+	jal strlen
+	add $s0, $v0, $zero
+	
+	add $s0, $s0, $a0
+	add $s1, $zero, $a1
+	add $s2, $a2, $zero
+	
+	addi $t1, $zero, 1
+	lb $t0, 0($s1)
+	sb $t0, 0($s0)	
+loop_strncat:
+	addi $s0, $s0, 1
+	addi $s1, $s1, 1
+	
+	lb $t0, 0($s1)
+	beq $t1, $s2, saida_strncat
+	sb $t0, 0($s0)
+	addi $t1, $t1, 1
+	j loop_strncat
+saida_strncat:
+	sb $zero, 0($s0)
+	addi $v0, $zero, 0 #Para não retornar nada 
+	
+	lw $s2, 0($sp)	
+	lw $s1, 4($sp)
+	lw $s0, 8($sp)
+	lw $ra, 12($sp)
+	addi $sp, $sp, 16
+	jr $ra #volta ao programa principal
+
 main:
 	la $a0, nome
 	la $a1, sobrenome
-	jal strcat
+	la $t0, count
+	lw $a2, 0($t0)
+	jal strncat
 	
 	#add $a0, $v0, $zero
 	li $v0, 4
